b930e46d43f23dff0694f0452170b3c924eb4354
Mauricio Oropeza <mauricio.3247@gmail.com>
2021-06-10 21:53:51 -0500

    improvements code to login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,123 +1,121 @@
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { firebaseClient } from '../helpers/client/firebase.client';
-import { useAuth } from '../hooks/useAuth';
-import { Button, Col, Input, Layout, notification, Row } from 'antd';
-import { Content } from 'antd/lib/layout/layout';
+import { Button, Input, Layout } from 'antd';
+import Text from 'antd/lib/typography/Text';
 import { ItemLayout } from '../components/ItemLayout';
-
 import {
   GoogleOutlined,
 } from '@ant-design/icons';
+
+
 import { useNotificationError } from '../hooks/useNotification';
-import Text from 'antd/lib/typography/Text';
+import { firebaseClient } from '../helpers/client/firebase.client';
+import { useAuth } from '../hooks/useAuth';
 
 export default (_props: any) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   const { user } = useAuth();
-  useEffect(()=> {
-    if(user && user.uid) {
-        window.location.href = '/'
+  useEffect(() => {
+    if (user && user.uid) {
+      window.location.href = '/'
     }
 
-  },[user])
+  }, [user])
 
   const [error, setError] = useNotificationError()
 
+  const signInWithEmailAndPassword = async () => {
+    try {
+      await firebaseClient
+        .auth()
+        .signInWithEmailAndPassword(email, pass);
+      window.location.href = '/';
+    } catch (error) {
+      setError({ title: 'Auth Error', ...error })
+    }
+
+  }
+
+  const signInWithProvider = async () => {
+    try {
+      await firebaseClient.auth().signInWithPopup(new firebaseClient.auth.GoogleAuthProvider())
+      window.location.href = '/';
+    } catch (error) {
+      setError({ title: 'Auth Error', ...error })
+    }
+
+  }
+
+  const createUserEmailAndPassword = async () => {
+    try {
+      await firebaseClient
+        .auth()
+        .createUserWithEmailAndPassword(email, pass);
+      window.location.href = '/';
+    } catch (error) {
+      setError({ title: 'Auth Error', ...error })
+    }
+
+  }
 
 
   return (
-    <Layout style={{backgroundColor: 'white'}}>
+    <Layout style={{ backgroundColor: 'white' }}>
 
-    <Layout.Content style={{ margin: '24px 16px 0', textAlign: 'center' }}>
+      <Layout.Content style={{ margin: '24px 16px 0', textAlign: 'center' }}>
 
-      <ItemLayout>
-        <h1>Login</h1>
-      </ItemLayout>
+        <ItemLayout>
+          <h1>Login</h1>
+        </ItemLayout>
 
-      <ItemLayout>
-        <Input
+        <ItemLayout>
+          <Input
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder={'Email'}
           />
-      </ItemLayout>
+        </ItemLayout>
 
 
-      <ItemLayout>
-      <Input
+        <ItemLayout>
+          <Input
             type={'password'}
             value={pass}
             onChange={(e) => setPass(e.target.value)}
             placeholder={'Password'}
           />
-      </ItemLayout>
+        </ItemLayout>
 
-      <ItemLayout>
-        <Button
-              onClick={async () => {
-                try {
-                  await firebaseClient
-                  .auth()
-                  .signInWithEmailAndPassword(email, pass);
-                window.location.href = '/';
-                } catch (error) {
-                  console.error('error', error)
-                  setError({title: 'Auth Error', ...error})
-                }
-
-              }}
-            >
-              Login user/password
+        <ItemLayout>
+          <Button
+            onClick={signInWithEmailAndPassword}>
+            Login user/password
             </Button>
-      </ItemLayout>
+        </ItemLayout>
 
-      <ItemLayout>
-      <Button
-          icon={<GoogleOutlined/>}
-          onClick={async () => {
-              try {
-                  await firebaseClient.auth().signInWithPopup(new firebaseClient.auth.GoogleAuthProvider())
-                  window.location.href = '/';
-              } catch (error) {
-                  console.error('error', error)
-                  
-              }
-
-          }}
-        >
-          Log in With Google
+        <ItemLayout>
+          <Button
+            icon={<GoogleOutlined />}
+            onClick={signInWithProvider}
+          >
+            Log in With Google
         </Button>
-      </ItemLayout>
+        </ItemLayout>
 
-      <ItemLayout>
-        <>
-        <Text>I don't have an account yet </Text>
-        <Button
-              onClick={async () => {
-                try {
-                  await firebaseClient
-                  .auth()
-                  .createUserWithEmailAndPassword(email, pass);
-                window.location.href = '/';
-                } catch (error) {
-                  console.error('error', error)
-                  setError({title: 'Auth Error', ...error})
-                }
-
-              }}
-            >
+        <ItemLayout>
+          <>
+            <Text>I don't have an account yet </Text>
+            <Button onClick={createUserEmailAndPassword}>
               Click to create an account
             </Button>
-        </>
+          </>
 
-      </ItemLayout>
+        </ItemLayout>
 
 
-</Layout.Content>
+      </Layout.Content>
 
-</Layout>
+    </Layout>
 
 
 
