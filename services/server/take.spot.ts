ec84e72ef0e560ea6005cebebad6053a16ff6239
Mauricio Oropeza <mauricio.3247@gmail.com>
2021-06-11 02:11:50 -0500

    fix userId variable it was a literal value

--- services/server/take.spot.ts
+++ services/server/take.spot.ts
@@ -1,9 +1,9 @@
 import firestore from '../../helpers/server/firestore.server';
 export const takeSpot = async (userId:string, newTime:string) => {
     try {
-        await firestore.collection('moto_register').doc('userId').delete()
+        await firestore.collection('moto_register').doc(userId).delete()
         if(newTime !== '') {
-            await firestore.collection('moto_register').doc('userId').set({
+            await firestore.collection('moto_register').doc(userId).set({
                 user_id: userId,
                 time: newTime
             })
